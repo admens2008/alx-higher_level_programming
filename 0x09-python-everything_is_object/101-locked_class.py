fe98e702e587a5f1cab93fe45128ca7de3be23dd
admens2008 <adjeimensah@gmail.com>
2024-01-05 04:34:06 -0800

    Python, Everything is Object

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
+"""overriding the '__setattr__' method
+"""
+
+
+'''
+File_name: 101-locked_class.py
+Project: 0x09-python-everything_is_object
+'''
+
+
 class LockedClass:
+    """
+    # Write a class LockedClass with no class or object attribute, that
+    # prevents the user from dynamically creating new instance attributes,
+    # except if the new instance attribute is called first_name..
+    # VARIABLE(" "):
+    # Locked Class Attributes(str): Low memory cost
+    # Return: Always 0, (Success).
+    """
+
     def __setattr__(self, name, value):
-        if name != 'first_name' or not hasattr(self, 'first_name'):
-            raise AttributeError(f"'LockedClass' object has no attribute '{name}'")
+        if not hasattr(self, 'first_name') and name != 'first_name':
+            raise AttributeError(
+                    "'LockedClass' object has no attribute '{}'".format(name))
         else:
-            object.__setattr__(self, name, value)
-
-# Example usage
-if __name__ == "__main__":
-    lc = LockedClass()
-    lc.first_name = "John"
-
-    try:
-        lc.last_name = "Snow"
-    except AttributeError as e:
-        print(f"[{e.__class__.__name__}] {e}")
-
+            object._setattr__(self, name, value)
